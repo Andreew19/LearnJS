cfdc96950cf75e36452bfa598b684caeb6161fce
Andreew19 <andrew_04@list.ru>
2022-11-03 18:40:12 +0300

    Chenge space on dash

---
+++ dist/index.c4775257.js
@@ -0,0 +1,12 @@
+// Chenge space on dash (1
+let string = "The is the midday show with Cheryl Waters";
+let urlFriendly = "";
+for(i = 0; i < string.length; i++)if (string[i] === " ") urlFriendly += "-";
+else urlFriendly += string[i];
+console.log(urlFriendly) // Chenge space on dash (2
+ // let string = "The is the midday show with Cheryl Waters";
+ // let urlFriendly =  string.replace(/ /g, "-" );
+ // console.log(urlFriendly)
+;
+
+//# sourceMappingURL=index.c4775257.js.map
--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-let a = 5
+// Chenge space on dash (1
 
-const sq = (a) => {
-  a*a
+let string = "The is the midday show with Cheryl Waters";
+let urlFriendly =  "";
+
+for(i=0; i<string.length; i++) {
+  if(string[i] === " ") {
+    urlFriendly += "-";
+  }
+  else{
+    urlFriendly += string[i]
+  }
 }
 
-const sumSq = (sum) => {
-  sum = sq + sq
-}
+console.log(urlFriendly)
 
+// Chenge space on dash (2
+
+// let string = "The is the midday show with Cheryl Waters";
+// let urlFriendly =  string.replace(/ /g, "-" );
+
+// console.log(urlFriendly)
